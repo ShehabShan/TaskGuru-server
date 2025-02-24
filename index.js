8f9b39df1ef75ec4d67801ff1ef1a189d62d0e20
ShehabShan <mdshehabsarar@gmail.com>
2025-02-25 02:19:36 +0600

    Chnage MongoDb link

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://taskguru-b25e4.web.app",
   "https://taskguru-b25e4.firebaseapp.com",
-  "https://*.railway.app", // Allow all Railway subdomains
+  "https://*.railway.app",
 ];
 
 app.use(
@@ -27,7 +27,8 @@ app.use(
 );
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d6z2i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d6z2i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d6z2i.mongodb.net/?retryWrites=true&w=majority&authSource=admin`;
 
 const client = new MongoClient(uri, {
   serverApi: {
